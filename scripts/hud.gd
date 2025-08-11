378c24559516a474f446ea1712f772a05f021ad1
elephantfive <dylan.chilcoat@gmail.com>
2025-08-10 21:51:25 -0500

    naming convention standardization

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -15,9 +15,9 @@ var itemlist = {
 }
 
 var inv_open = false
-@onready var inventory_button = $inventory_button
-@onready var inventory_box = %inventory_box
-@onready var item_desc = %item_desc
+@onready var inventory_button = $InventoryButton
+@onready var inventory_box = %InventoryBox
+@onready var item_desc = %ItemDesc
 const ITEM = preload("res://main/items/item.tscn")
 
 func additem(item):
